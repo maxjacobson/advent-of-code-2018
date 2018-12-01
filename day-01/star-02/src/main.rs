4c6db6966df9cb5097f055278f898549b7397f6d
Max Jacobson <max@hardscrabble.net>
2018-12-01 22:08:39 +0000

    Add day-01 star-02

--- day-01/star-02/src/main.rs
+++ day-01/star-02/src/main.rs
@@ -1,3 +1,24 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let mut frequency_occurences = HashSet::new();
+
+    let changes = input
+        .split_whitespace()
+        .map(|line| line.parse::<i32>().unwrap())
+        .cycle();
+
+    frequency_occurences.insert(0);
+
+    let mut frequency = 0;
+    for change in changes {
+        frequency += change;
+        if frequency_occurences.contains(&frequency) {
+            println!("Result: {}", frequency);
+            break;
+        } else {
+            frequency_occurences.insert(frequency);
+        }
+    }
 }

16288391ba5384bcdd61b1bbe6147b8744d5a8ca
Max Jacobson <max@hardscrabble.net>
2018-12-01 16:34:19 -0500

    Add day 1 star 1

--- day-01-star-01/src/main.rs
+++ day-01-star-01/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let result: i32 = input
+        .split_whitespace()
+        .fold(0, |acc, line| acc + line.parse::<i32>().unwrap());
+
+    println!("Result: {}", result);
 }

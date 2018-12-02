ea6de34fd7131bc3aff30ff1c3177f8c262daaa7
Max Jacobson <max@hardscrabble.net>
2018-12-02 22:02:45 +0000

    Add day 2 star 1 solution

--- day-02/star-01/src/main.rs
+++ day-02/star-01/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+fn any_letter_exactly_n_times(id: &str, n: i32) -> bool {
+    let map = HashMap::new();
+
+    id.chars()
+        .fold(map, |mut acc, c| {
+            *acc.entry(c).or_insert(0) += 1;
+            acc
+        }).values()
+        .any(|v| v == &n)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+
+    let mut exactly_two_count = 0;
+    let mut exactly_three_count = 0;
+
+    for line in input.split_whitespace() {
+        if any_letter_exactly_n_times(line, 2) {
+            exactly_two_count += 1;
+        }
+
+        if any_letter_exactly_n_times(line, 3) {
+            exactly_three_count += 1;
+        }
+    }
+
+    println!("Result: {}", exactly_three_count * exactly_two_count);
 }

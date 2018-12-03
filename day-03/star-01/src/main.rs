e69ec5c661f3dc78ae215eff4b9dac316163a329
Max Jacobson <max@hardscrabble.net>
2018-12-03 23:48:01 +0000

    Add day 3 star 1

--- day-03/star-01/src/main.rs
+++ day-03/star-01/src/main.rs
@@ -1,3 +1,62 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashMap;
+
+type Point = (usize, usize);
+type Count = usize;
+
+#[derive(Debug)]
+struct Claim {
+    id: String,
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+}
+
+impl Claim {
+    fn from_description(description: &str) -> Self {
+        lazy_static! {
+            static ref DESCRIPTION_REGEX: Regex =
+                Regex::new(r"^(#\d+) @ (\d+),(\d+): (\d+)x(\d+)$").unwrap();
+        }
+
+        let caps = DESCRIPTION_REGEX.captures(description).unwrap();
+        let id = caps.get(1).unwrap().as_str().to_string();
+        let x = caps.get(2).unwrap().as_str().parse().unwrap();
+        let y = caps.get(3).unwrap().as_str().parse().unwrap();
+        let width = caps.get(4).unwrap().as_str().parse().unwrap();
+        let height = caps.get(5).unwrap().as_str().parse().unwrap();
+
+        Claim {
+            id,
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let claims = include_str!("../input.txt")
+        .lines()
+        .map(|line| Claim::from_description(line));
+
+    let mut claims_counter: HashMap<Point, Count> = HashMap::new();
+
+    for claim in claims {
+        for x in (claim.x)..(claim.x + claim.width) {
+            for y in (claim.y)..(claim.y + claim.height) {
+                *claims_counter.entry((x, y)).or_insert(0) += 1
+            }
+        }
+    }
+
+    let result = claims_counter.values().filter(|v| *v >= &2).count();
+
+    println!("Result: {}", result);
 }
